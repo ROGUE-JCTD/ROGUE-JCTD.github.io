5791864dbd692923427ef2b4358e5328932617fd
Tyler Garner <garnertb@gmail.com>
2014-03-05 09:23:01 -0500

    Automatically generate permalinks for anchors.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+$(document).ready(function(){
+
+    // Add permalinks to anchors.
+    _.each($('.anchor'), function(el) {
+        var el = $(el);
+        var parent = el.parents();
+        var permalink = $('<a>').html('¶').attr({href:'#' + el.attr('name'),
+                                                 class:'headerlink',
+                                                 title:"Permalink to this headline"});
+        parent.append(permalink);
+    });
+
+});
